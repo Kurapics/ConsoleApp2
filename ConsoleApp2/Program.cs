9a3df399b3aa503368bd6acdbf0a6bc88fcb4734
Компьютер <@DESKTOP-0RODP2R>
2023-04-25 22:28:28 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿double[] R = new double[25];
+Random r = new Random();
+
+for (int i = 0; i < R.Length; i++)
+    R[i] = r.Next(-100, 100);
+
+foreach (int i in R)
+    Console.Write(i + " ");
+
+for (int i = 0;i < R.Length; i++)
+{
+    if (R[i] < 0) R[i] = Math.Pow(R[i], 2);
+    else if (R[i] > 0) R[i] = R[i] + 7;
+    else R[i] = 0;
+    Console.Write(R[i] + " ");
+}
